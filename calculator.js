1932f13e453505e1779cd34873d2e142d2aa766d
CHAMRTHI VENKATESH <160413645+venkatesh767408@users.noreply.github.com>
2024-05-29 20:23:42 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,24 +1,5 @@
-/*document.addEventListener("DOMContentLoaded", function() {
-    const display = document.getElementById("display");
 
-    window.appendToDisplay = function(input) {
-        if (input === 'c') {
-            // Clear the display
-            display.value = '';
-        } else if (input === '=') {
-            // Evaluate the expression
-            try {
-                display.value = eval(display.value);
-            } catch (e) {
-                display.value = 'Error';
-            }
-        } else {
-            // Append input to the display
-            display.value += input;
-        }
-    };
-});
-*/
+    
 const display=document.getElementById("display");
 function appendToDisplay(input){
     display.value +=input;
